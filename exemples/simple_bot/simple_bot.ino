d11586e74f1846631ea3ae8fc5f34dd762e39515
TiagoPintoAlves <tiago.alves718@gmail.com>
2016-08-18 00:28:25 -0300

    feat(exemples): adicionar exemplo .ino

---
+++ exemples/simple_bot/simple_bot.ino
@@ -0,0 +1,28 @@
+#include <MyRobot.h>
+
+MyRobot bot;
+
+int AnalogPins[4] = { 0, 1, 2, 3 };
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Initializing basic some stuff:
+  bot.setupServo(10, 11);
+  bot.setupUltra(12, 13);
+  bot.setupAnalogSensors(AnalogPins, INPUT);
+}
+
+void loop() {
+  // Using servos:
+  bot.goAhead();
+  delay(300);
+  bot.stopServo();
+
+  // Using Ultrassonic:
+  Serial.println( bot.getDistancia() );
+
+  // Using Analog Sensor:
+  Serial.println( bot.getAnalogRead(AnalogPins[0]) );
+  
+}
